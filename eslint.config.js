482130c827e231a323c7b21c4c7aec6805acab22
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2024-06-19 17:45:30 +0200

    feat: include eslint plugin for prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import pluginJs from "@eslint/js";
 import tsEslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import prettierEslint from "eslint-config-prettier";
 import globals from "globals";
 
 export default [
   pluginJs.configs.recommended,
   ...tsEslint.configs.recommended,
+  prettierEslint,
   {
     files: ["**/*.{ts,jsx}"],
     plugins: {pluginReact},
